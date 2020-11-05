b4e80c9b5fd8e5cc708de854e9a31216ae7750a2
soohyun <soohyun6879@naver.com>
2020-11-05 15:49:43 +0900

    Update: User.java 수정

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,19 +1,38 @@
 package domain;
 
+import jdk.jfr.Enabled;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class User {
-    private Long id;
-    private Long pw;
+
+    private Long id; //pk
+    private String user_id;
+    private String password;
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
-    public Long getPw() {
-        return pw;
+
+    public String getUser_id() {
+        return user_id;
     }
-    public void setPw(Long pw) {
-        this.pw = pw;
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
